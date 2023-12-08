d1f81cd25ae1493d02f02979ec38640fb13cbdb7
1878 <1878@LAPTOP-95N5CJET>
2023-12-09 01:59:27 +0700

    upload

---
+++ Test Suites/Test Suit StarBridges.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Test Suit StarBridges</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>436b1ede-bba2-403a-b61a-a9c7a24379bf</testSuiteGuid>
+   <testCaseLink>
+      <guid>0b249fe2-5ccb-4662-817e-68c7ebe66ccf</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Open StarBridges</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
